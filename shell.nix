d8e3a9956d89b687cdd914cd7e84734516d9dae0
Ezra Singh <ezra@liontech.nyc>
2024-09-07 18:28:28 -0400

    Added nix-shell support

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  inherit (pkgs) stdenv;
+
+in pkgs.mkShell {
+  name = "latex-development-environment";
+  buildInputs = with pkgs; [ texliveFull just ];
+}
